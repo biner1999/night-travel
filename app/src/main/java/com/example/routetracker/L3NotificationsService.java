95f6415f598ad7ac78dfb48cb5be021602c751ae
biner1999 <biner1999@gmail.com>
2020-03-01 22:28:35 +0000

    almost fully functional L3 and L4 notifications, needs correct text for the SMS and L3 needs to be able to send to multiple emergency contacts

---
+++ app/src/main/java/com/example/routetracker/L3NotificationsService.java
@@ -0,0 +1,139 @@
+package com.example.routetracker;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.IBinder;
+import android.telephony.SmsManager;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.maps.GoogleMap;
+
+//import static com.example.routetracker.App.CHANNEL_ID;
+
+public class L3NotificationsService extends Service {
+
+    public static final String CHANNEL_ID_1 = "Foreground channel";
+    public static final String CHANNEL_ID_2 = "Alerts channel";
+    public static final String GROUP_ID_1 = "Group 1";
+    private String phoneNumber = "07706473014";
+    private String name = "Bart";
+    private String timeLate = "20";
+    private String destination = "21 Street";
+    private String startingLocation = "22 Street";
+    private String textMessage = "This is an automated text sent by RouteTracker from " + name + ". I am currently " + timeLate + " minutes late on my journey from " + startingLocation + " to " + destination + ". Try to get a hold of me.";
+
+    public void sendSMS() {
+        boolean mSMSPermissionGranted = false;
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.SEND_SMS)
+                == PackageManager.PERMISSION_GRANTED) {
+            mSMSPermissionGranted = true;
+        } else {
+
+        }
+
+        if (mSMSPermissionGranted) {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNumber, null, textMessage, null, null);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startID) {
+        sendSMS();
+        Intent notificationIntent = new Intent(this, LoginActivity.class); //will call login activity later
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID_2)
+                .setSmallIcon(R.drawable.ic_warning)
+                .setContentTitle("Level 3 Alert")
+                .setContentText("A text has been sent to " + "-name-." + " Enter the Route Tracker to give yourself more time")
+                .setColor(Color.RED)
+                .setCategory(NotificationCompat.CATEGORY_ALARM)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                //.setFullScreenIntent(contentIntent, true)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .build();
+        notification.flags |= Notification.FLAG_INSISTENT;
+        notification.flags |= Notification.FLAG_ONGOING_EVENT;
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(2, notification);
+
+/*
+        Window window = .getWindow();
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+*/
+
+
+        return START_REDELIVER_INTENT;
+
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+}
+
+
+/*    private void addNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Level 1 Alert")
+                .setContentText("Level 1 Alert")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(1, builder.build());
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+    }
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }*/
+
