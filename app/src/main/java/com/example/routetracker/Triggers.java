60f3936cff4dcdb47f4fa542b89c5a46dd6ce2e3
biner1999 <biner1999@gmail.com>
2020-03-16 16:51:08 +0000

    Trigger class now fully works

--- app/src/main/java/com/example/routetracker/Triggers.java
+++ app/src/main/java/com/example/routetracker/Triggers.java
@@ -1,21 +1,45 @@
 package com.example.routetracker;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 
-public class Triggers {
+public class Triggers extends Service {
 
     // First Time Trigger //
-    public void FirstTriggerStart(long time ) {
-        TimerTask task = new TimerTask() {
-            public void run() {
-                System.out.println("PLACE CALL FOR THE FIRST TRIGGER HERE");
-            }
-        };
-        Timer timer = new Timer("Timer");
-
+    public void FirstTriggerStart(long time) {
         long delay = Math.round(time*1.25) + 5000;
-        timer.schedule(task, delay);
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+                startL1Service();
+        }, 5000);
+
+
+
+    }
+    public void startL1Service() {
+        Intent L1ServiceIntent = new Intent(this, L1NotificationsService.class);
+        startService(L1ServiceIntent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        int timeID = intent.getIntExtra("timeID", 0);
+        FirstTriggerStart(timeID);
+        return START_REDELIVER_INTENT;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
