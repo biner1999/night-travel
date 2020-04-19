d95e12b96316082af373435e9f8aeda1ffa083ee
Jack Davies <daviesj142@cardiff.ac.uk>
2020-04-19 17:41:39 +0100

    Fixed numerical time bug

--- app/src/main/java/com/example/routetracker/homescreenActivity.java
+++ app/src/main/java/com/example/routetracker/homescreenActivity.java
@@ -36,6 +36,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
